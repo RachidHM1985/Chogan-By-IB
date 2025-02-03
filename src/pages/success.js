bd408fe69d50e46f9f49308a0e790607d224f189
rachid hachem <rachid.hache@acelys.fr>
2025-02-03 21:13:14 +0100

    succes paiement

--- src/pages/success.js
+++ src/pages/success.js
@@ -10,42 +10,45 @@ const Success = () => {
 
   useEffect(() => {
     const { session_id, payment_intent, status } = router.query;
-console.log("status : ",status)
-console.log("payment_intent : ",payment_intent)
-console.log("session_id : ",session_id)
+
+    if (!status) return; // Wait until payment_intent is available
+
+    console.log("status : ", status);
+    console.log("payment_intent : ", payment_intent);
+    console.log("session_id : ", session_id);
 
     if (payment_intent.status === 'succeeded') {
-      setLoading(false); // Si le paiement est réussi, arrête le chargement.
+      setLoading(false); // If payment is successful, stop loading
     } else {
-      router.push('/echec'); // Si l'état n'est pas 'succeeded', redirige vers la page d'échec.
+      router.push('/echec'); // Redirect to failure page if payment is not succeeded
     }
   }, [router.query]);
 
   return (
     <>
-    <Header />
-    <div>
-      {loading ? (
-        <Typography variant="h6" align="center">
-          Vérification du paiement...
-        </Typography>
-      ) : (
-        <div>
-          <Typography variant="h4" align="center" color="green">
-            Paiement réussi !
-          </Typography>
+      <Header />
+      <div>
+        {loading ? (
           <Typography variant="h6" align="center">
-            Merci pour votre commande, votre paiement a été effectué avec succès. Vous allez recevoir un mail de confirmation.
+            Vérification du paiement...
           </Typography>
-          <div style={{ textAlign: 'center', marginTop: '20px' }}>
-            <Button variant="contained" color="primary" onClick={() => router.push('/')}>
-              Retour à la page d'accueil
-            </Button>
+        ) : (
+          <div>
+            <Typography variant="h4" align="center" color="green">
+              Paiement réussi !
+            </Typography>
+            <Typography variant="h6" align="center">
+              Merci pour votre commande, votre paiement a été effectué avec succès. Vous allez recevoir un mail de confirmation.
+            </Typography>
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+              <Button variant="contained" color="primary" onClick={() => router.push('/')}>
+                Retour à la page d'accueil
+              </Button>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-    <Footer />
+        )}
+      </div>
+      <Footer />
     </>
   );
 };
