1e39ebbae15ecb5737daba5b96b8e55d070d3d73
rachid hachem <rachid.hache@acelys.fr>
2025-02-04 00:07:26 +0100

    webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,22 +1,22 @@
-// pages/api/webhook.js
 import { buffer } from 'micro';
 import Stripe from 'stripe';
 import { supabase } from '../../supabaseClient';
 
-// Initialise Stripe avec votre clé secrète
+// Initialisation de Stripe avec la clé secrète
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27',
 });
 
-// Définir le type de contenu comme raw pour pouvoir récupérer le body de l'événement Stripe
+// Configuration pour désactiver le body parser de Next.js afin de recevoir des données brutes
 export const config = {
   api: {
-    bodyParser: false, // Ne pas parser le corps
+    bodyParser: false, // Ne pas parser le corps de la requête
   },
 };
 
 const handleWebhook = async (req, res) => {
   if (req.method !== 'POST') {
+    console.error(`Méthode non autorisée: ${req.method}`);
     return res.status(405).send('Method Not Allowed');
   }
 
@@ -25,17 +25,18 @@ const handleWebhook = async (req, res) => {
     const reqBuffer = await buffer(req);
     const sig = req.headers['stripe-signature'];
 
-    // Vérifiez la signature de Stripe pour valider l'intégrité de la requête
+    // Vérifier la signature Stripe pour valider l'intégrité de la requête
     const event = stripe.webhooks.constructEvent(
       reqBuffer,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET
     );
 
+    // Gérer l'événement checkout.session.completed
     if (event.type === 'checkout.session.completed') {
       const session = event.data.object;
 
-      // Récupérez les données de la commande
+      // Récupérer les données pertinentes de la session
       const { customer_email, customer_name, amount_total, shipping, metadata } = session;
 
       // Enregistrer la commande dans Supabase
@@ -47,8 +48,8 @@ const handleWebhook = async (req, res) => {
             user_email: customer_email,
             user_phone: shipping.phone,
             user_address: shipping.address.line1,
-            total_amount: amount_total / 100, // Stripe retourne le montant en cents
-            delivery_fee: metadata.deliveryFee || 0, // Optionnel, si vous avez cette information dans les métadonnées
+            total_amount: amount_total / 100, // Montant en cents
+            delivery_fee: metadata.deliveryFee || 0, // Si les métadonnées contiennent une livraison
             order_status: 'completed',
           },
         ]);
