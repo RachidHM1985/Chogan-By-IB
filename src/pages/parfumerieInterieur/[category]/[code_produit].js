5900015f650d902b666c918d0f8c497e322b17e6
rachid hachem <rachid.hache@acelys.fr>
2025-03-25 15:58:47 +0100

    TITRE PARFUMERIE

--- src/pages/parfumerieInterieur/[category]/[code_produit].js
+++ src/pages/parfumerieInterieur/[category]/[code_produit].js
@@ -73,8 +73,7 @@ const ProduitDetailPage = () => {
               <>
                 <Row className="d-flex justify-content-center align-items-center">
                   <Col xs={12} md={6} className="text-left d-flex flex-column align-items-center">
-                    <Typography variant="h6" sx={{ fontWeight: '600', textAlign: 'center' }}>{produit.categorie}</Typography>
-                    <Typography variant="h6" sx={{ fontWeight: '600', textAlign: 'center' }}>{produit.nom_produit}</Typography>
+                    <Typography variant="h6" sx={{ fontWeight: '600', textAlign: 'center' }}>Diffuseur: {produit.categorie}</Typography>
                     {produit.code_produit && (
                       <img src={`../../images/products/${produit.code_produit}.jpg`} alt={produit.description} className="img-fluid" style={{ maxWidth: '90%', height: 'auto', borderRadius: '10px' }} />
                     )}
