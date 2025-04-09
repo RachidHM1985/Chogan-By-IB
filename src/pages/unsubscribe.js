a57de69a817b5d007aa9a0cf33956287c8566cc2
rachid hachem <rachid.hache@acelys.fr>
2025-04-09 15:48:36 +0200

    trace de désabonnement du prospect

--- src/pages/unsubscribe.js
+++ src/pages/unsubscribe.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Container, Typography, CircularProgress, Alert } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Analytics } from '@vercel/analytics'; // Assurez-vous que vous avez importé Vercel Analytics
 
 // Créer un thème personnalisé pour la police Montserrat
 const theme = createTheme({
@@ -27,13 +28,32 @@ const Unsubscribe = () => {
           if (response.ok) {
             setMessage(data.message); // Message de succès
             setSeverity('success'); // Type d'alerte pour succès
+
+            // Suivi de l'événement de désinscription dans Vercel Analytics
+            Analytics.track('Unsubscribe Successful', {
+              email: email, // Inclure l'email pour l'événement
+              message: data.message, // Inclure un message d'accompagnement si nécessaire
+            });
+
           } else {
             setMessage(data.message); // Message d'erreur
             setSeverity('error'); // Type d'alerte pour erreur
+
+            // Suivi de l'événement d'erreur de désinscription dans Vercel Analytics
+            Analytics.track('Unsubscribe Failed', {
+              email: email,
+              message: data.message,
+            });
           }
         } catch (error) {
           setMessage('Une erreur s\'est produite. Veuillez réessayer plus tard.');
           setSeverity('error'); // Type d'alerte pour erreur
+
+          // Suivi de l'événement d'erreur général dans Vercel Analytics
+          Analytics.track('Unsubscribe Error', {
+            email: email,
+            error: error.message, // Inclure le message d'erreur
+          });
         } finally {
           setLoading(false);
         }
